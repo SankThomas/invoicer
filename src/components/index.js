f65c393e128bf74e3802a7873d859dcf94450578
Thomas Sankara <thomassankara74@gmail.com>
2021-08-25 20:06:32 +0300

    entry file

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./Header"
+import Person from "./Person"
+import Client from "./Client"
+import InvoiceNumber from "./InvoiceNumber"
+import InvoiceDetails from "./InvoiceDetails"
+import Notes from "./Notes"
+import Footer from "./Footer"
+import SendModal from "./SendModal"
+
+export {
+  Header,
+  Person,
+  Client,
+  InvoiceNumber,
+  InvoiceDetails,
+  Notes,
+  Footer,
+  SendModal,
+}
