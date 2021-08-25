a4072778a0152752befd1b9dff5ca949e75d4a20
Thomas Sankara <thomassankara74@gmail.com>
2021-08-25 20:06:08 +0300

    Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="px-5 mt-10 lg:px-0 border-t border-gray-300 pt-5 pb-10">
+        <ul className="flex items-center justify-center flex-wrap">
+          <li className="font-bold mx-2">
+            Person's name: address <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Email: <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Phone: <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Website: <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Bank: <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Account Number: <span className="font-light">details</span>
+          </li>
+          <li className="font-bold mx-2">
+            Account Holder: <span className="font-light">details</span>
+          </li>
+        </ul>
+      </footer>
+    </>
+  )
+}
