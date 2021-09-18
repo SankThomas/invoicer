c963b453d95dd217537746b8aef11820a56a3173
Thomas Sankara <thomassankara74@gmail.com>
2021-09-18 08:26:43 +0300

    Invoice table details component

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,42 +1,7 @@
-import { useState } from "react"
-
-export default function Client() {
-  const [clientName, setClientName] = useState("Client's name")
-  const [clientAddress, setClientAddress] = useState("Client's address")
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
-
+export default function Client({ clientName, clientAddress }) {
   return (
     <>
       <article className="flex flex-col items-start justify-between px-5 lg:px-0 text-center mt-10">
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <label htmlFor="clientName" className="font-bold">
-            Client's name
-          </label>
-          <input
-            type="text"
-            name="clientName"
-            id="clientName"
-            placeholder="Client's name"
-            required
-            value={clientName}
-            onChange={(e) => setClientName(e.target.value)}
-          />
-          <label htmlFor="clientAddress" className="font-bold">
-            Client's Address
-          </label>
-          <input
-            type="text"
-            name="clientAddress"
-            id="clientAddress"
-            placeholder="Client's address"
-            value={clientAddress}
-            onChange={(e) => setClientAddress(e.target.value)}
-          />
-        </form>
-
         <h3 className="uppercase font-bold text-2xl">{clientName}</h3>
         <p>{clientAddress}</p>
       </article>
