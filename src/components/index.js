46848ec4871654d9dc219cd1298043ec4f044915
Thomas Sankara <thomassankara74@gmail.com>
2021-09-19 19:08:16 +0300

    index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Header from "./Header"
 import Person from "./Person"
 import Client from "./Client"
+import InvoiceForm from "./InvoiceForm"
 import InvoiceNumber from "./InvoiceNumber"
 import InvoiceDetails from "./InvoiceDetails"
+import Details from "./Details"
 import Notes from "./Notes"
 import Footer from "./Footer"
 import SendModal from "./SendModal"
@@ -11,8 +13,10 @@ export {
   Header,
   Person,
   Client,
+  InvoiceForm,
   InvoiceNumber,
   InvoiceDetails,
+  Details,
   Notes,
   Footer,
   SendModal,
