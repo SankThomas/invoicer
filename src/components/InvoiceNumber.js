77119600093e98bff3e67a924d21aa9b371503bf
Thomas Sankara <thomassankara74@gmail.com>
2021-09-25 20:47:55 +0300

    InvoiceNumber component

--- src/components/InvoiceNumber.js
+++ src/components/InvoiceNumber.js
@@ -1,46 +1,10 @@
-import { useState } from "react"
-
-export default function InvoiceNumber() {
-  const [invoiceNumber, setInvoiceNumber] = useState("")
-  const [invoiceDate, setInvoiceDate] = useState("")
-  const [dueDate, setDueDate] = useState("")
-
+export default function InvoiceNumber({ invoiceNumber, dueDate, invoiceDate }) {
   return (
     <>
       <article className="flex flex-col items-end px-5 justify-between lg:px-0 mt-10">
-        <p className="font-bold">
-          Invoice number:{" "}
-          <input
-            type="text"
-            value={invoiceNumber}
-            onChange={(e) => setInvoiceNumber(e.target.value)}
-          />{" "}
-          <span className="font-light">{invoiceNumber}</span>
-        </p>
-        <p className="font-bold">
-          Invoice date:{" "}
-          <input
-            type="date"
-            name="date"
-            id="date"
-            required
-            value={invoiceDate}
-            onChange={(e) => setInvoiceDate(e.target.value)}
-          />
-          <span className="font-light">{invoiceDate}</span>
-        </p>
-        <p className="font-bold">
-          Due date:{" "}
-          <input
-            type="date"
-            name="date"
-            id="date"
-            required
-            value={dueDate}
-            onChange={(e) => setDueDate(e.target.value)}
-          />{" "}
-          <span className="font-light">{dueDate}</span>
-        </p>
+        <p>Invoice Number: {invoiceNumber}</p>
+        <p>Invoice Date: {invoiceDate}</p>
+        <p>Due date: {dueDate}</p>
       </article>
     </>
   )
